11bce512e5329e3d3ed8a1c47cd03b1bcb36fe39
Seshadri Vishwanatham <seshadri.vishwanatham@yexledomain.com>
2025-05-13 14:46:33 +0530

    Changed stepdefinition name

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources/featureFiles"}
-        ,glue={"stepdefination"},
+        ,glue={"stepdefinition"},
         monochrome = true,
         plugin = {"pretty",
                 "html:target/cucumber-reports/cucumber.html"}
